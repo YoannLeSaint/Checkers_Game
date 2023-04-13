ca55f66bc5b82812e6216be497bf8cb6ac3edfb2
Nomist2sr <simon.tessier@epitech.eu>
2023-04-13 17:24:34 +0300

    yes

--- src/utils.c
+++ src/utils.c
@@ -214,13 +214,11 @@ void showChecker(char checker[10][10]){
     printf("    1   2   3   4   5   6   7   8   9   10\n");
     printf("  +---+---+---+---+---+---+---+---+---+---+\n");
     for(int i=0;i<10;i++){
-        //printf("  |   |   |   |   |   |   |   |   |   |   |\n");
         if(i<9){printf("%d ",i+1);} else{printf("%d",i+1);}
         for(int j=0;j<10;j++){
             printf("| %c ",checker[i][j]);
         }
         printf("|\n");
-        //printf("  |   |   |   |   |   |   |   |   |   |   |\n");
         printf("  +---+---+---+---+---+---+---+---+---+---+\n");
     }
 }
